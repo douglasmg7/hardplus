912776e6cfe9024da6fc19b4e49f2f7e54edbc21
Douglas Gomes <douglasmg7@gmail.com>
2016-08-25 14:48:08 -0300

    sucess curl

--- produtos_vtabelas/data.sh
+++ produtos_vtabelas/data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mysql --defaults-file=/home/douglasmg7/.my.cnf -h hardplus.com.br hardplu1_loja << EOF
+mysql --defaults-file=/home/douglasmg7/.my.cnf -t -h hardplus.com.br hardplu1_loja << EOF
 # use test;
 # show tables;
 # desc produtos_vtabela;
