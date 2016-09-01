5efbb42c3f17420f995611ce5fab66b97643aa57
Douglas Gomes <douglasmg7@gmail.com>
2016-09-01 15:43:12 -0300

    mondodb bulk

---
+++ allnations/tDb.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+// maybe lost some data if the two server have diferent time
+
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+const assert = require('assert');
+const mongo = require('mongodb').MongoClient;
+// Mongodb configuration
+const mongoUrl = 'mongodb://localhost:27017/test';
+
+console.log('script started');
+
+mongo.connect(mongoUrl, (err, db)=>{
+	assert.equal(null, err);
+
+  let products = [];
+  products.push({code: 1, name: 'board', loja: 'sp', price: 111});
+  products.push({code: 2, name: 'mouse', loja: 'sp', price: 222});
+  // products.push({_id: 3, name: 'memory', price: 44});
+
+  let bulk = db.collection('produtct').initializeUnorderedBulkOp();
+  bulk.find({code: '1', loja: 'sp'}).upsert().updateOne({code: '1', loja: 'sp', name: 'mouse', price: '111'});
+  bulk.find({code: '4', loja: 'rj'}).upsert().update({$set: {code: '2', loja: 'ma', price: '223'}});
+  bulk.find({code: '3', loja: 'sp'}).upsert().updateOne({code: '3', loja: 'sp', name: 'memory', price: '333'});
+  bulk.execute((err, r)=>{
+    assert.equal(null, err);
+    // console.log(r);
+    console.log(r.toJSON());
+    // console.log(r.nInserted);
+    // console.log(r.nInserted);
+    db.close();
+  });
+
+	// // db.collection('produtct').insertMany(products, (err, r)=>{
+	// db.collection('produtct').updateMany({code: 1, loja: 'rj'}, {$set: {name: 'board', price: 3}}, {upsert: true}, (err, r)=>{
+	// 	assert.equal(null, err);
+  //   console.log(`insert count: ${r.insertedCount}`);
+	// 	console.log(r);
+	// 	db.close();
+	// });
+});
--- allnations/tReq.js
+++ allnations/tReq.js
@@ -9,14 +9,16 @@ const request = require('request');
 const assert = require('assert');
 
   let now = new Date();
-  let lastQuery = new Date();
+  let lastQuery = new Date('2015');
+  console.log(lastQuery.toISOString());
   // lastQuery.setMinutes(lastQuery.getMinutes() - 10);
-  lastQuery.setHours(lastQuery.getHours() - 12);
+  // lastQuery.setHours(lastQuery.getHours() - 12);
   // console.log(`Last query: ${lastQuery.toISOString()}`);
   // console.log(lastQuery.toISOString());
   // console.log(lastQuery.getTimezoneOffset());
 
-  if (false) {
+
+  if (true) {
     return;
   }
 
@@ -26,11 +28,11 @@ const assert = require('assert');
 
   request.get(url, (err, res, body) => {
   	assert.equal(null, err);
-    console.log(res);
+    // console.log(res);
     // Write xml result to file.
-    // let xmlFile = __dirname + '/' + lastQuery.toISOString() + '.xml';
-    // fs.writeFile(xmlFile, body, (err)=>{
-    //   assert.equal(null, err);
-    //   console.log(`${xmlFile} saved`);
-    // });
+    let xmlFile = __dirname + '/' + lastQuery.toISOString() + '.xml';
+    fs.writeFile(xmlFile, body, (err)=>{
+      assert.equal(null, err);
+      console.log(`${xmlFile} saved`);
+    });
   });
