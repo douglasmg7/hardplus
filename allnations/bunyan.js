9cd4ffd6ef90aace091597ff48eede0a280f7bfa
Douglas Gomes <douglasmg7@gmail.com>
2016-09-02 13:42:18 -0300

    creating timer

--- allnations/bunyan.js
+++ allnations/bunyan.js
@@ -21,6 +21,49 @@ let log = bunyan.createLogger({
   // src: true
 });
 
+let timer = {
+  start (label) {
+    timer[label] = process.hrtime();
+  },
+
+  // n - nanoseconds
+  // m - miliseconds
+  // s - seconds
+  stop (label, option) {
+    if (this[label]) {
+      // Elapsed time in nanoseconds.
+      let elapsedTime = process.hrtime(this[label]);
+      let elapsedTimeNs = elapsedTime[0] * 1e9 + elapsedTime[1];
+      // unset this[label];
+      switch (option) {
+        case 'n':
+          return elapsedTimeNs + 'ns';
+        case 'm':
+          return Math.round(elapsedTimeNs / 1e6) + 'ms';
+        case 's':
+          return Math.round(elapsedTimeNs / 1e9) + 's';
+        default:
+          return Math.round(elapsedTimeNs / 1e6) + 'ms';
+      }
+    }
+    else {
+      return (`${label} timer not started`);
+    }
+  }
+};
+
+
+
+let t = process.hrtime();
+log.info(t);
+// timer.start('t1');
+setTimeout(()=>{
+  // log.info('Timer t1: ' + timer.stop('t2'));
+  let t_end = process.hrtime(t);
+  let t_f = t_end[0] * 1e9 + t_end[1];
+  log.info(Math.round(t_f / 1e6));
+}, 2000);
+
 log.error('Err logged');
 log.warn('Warn logged');
 log.info('Info logged');
