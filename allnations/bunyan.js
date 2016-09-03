b770fb782106be26227f475ad650183ca319ad09
Douglas Gomes <douglasmg7@gmail.com>
2016-09-02 22:04:44 -0300

    new module timer

--- allnations/bunyan.js
+++ allnations/bunyan.js
@@ -3,7 +3,7 @@
 
 const path = require('path');
 const bunyan = require('bunyan');
-
+const timer = require('./timer');
 const appName = path.basename(__filename);
 
 let log = bunyan.createLogger({
@@ -21,48 +21,10 @@ let log = bunyan.createLogger({
   // src: true
 });
 
-let timer = {
-  start (label) {
-    timer[label] = process.hrtime();
-  },
-
-  // n - nanoseconds
-  // m - miliseconds
-  // s - seconds
-  stop (label, option) {
-    if (this[label]) {
-      // Elapsed time in nanoseconds.
-      let elapsedTime = process.hrtime(this[label]);
-      let elapsedTimeNs = elapsedTime[0] * 1e9 + elapsedTime[1];
-      // unset this[label];
-      switch (option) {
-        case 'n':
-          return elapsedTimeNs + 'ns';
-        case 'm':
-          return Math.round(elapsedTimeNs / 1e6) + 'ms';
-        case 's':
-          return Math.round(elapsedTimeNs / 1e9) + 's';
-        default:
-          return Math.round(elapsedTimeNs / 1e6) + 'ms';
-      }
-    }
-    else {
-      return (`${label} timer not started`);
-    }
-  }
-};
-
-
-
-let t = process.hrtime();
-log.info(t);
-// timer.start('t1');
+timer.start('f1');
 setTimeout(()=>{
-  // log.info('Timer t1: ' + timer.stop('t2'));
-  let t_end = process.hrtime(t);
-  let t_f = t_end[0] * 1e9 + t_end[1];
-  log.info(Math.round(t_f / 1e6));
-}, 2000);
+  log.info(timer.stop('f1'));
+}, 500);
 
 log.error('Err logged');
 log.warn('Warn logged');
