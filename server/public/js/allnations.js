1846c423965ba30abd34d7f8b91c0a97f6b47385
Douglas Gomes <douglasmg7@gmail.com>
2016-09-09 15:12:33 -0300

    backbone working with bower

--- server/public/js/allnations.js
+++ server/public/js/allnations.js
@@ -1,9 +1,21 @@
 // 'use strict';
-var $ = require('jquery')(window);
-var Backbone = require('backbone');
-Backbone.$ = $;
 
-// comment.
+// var $ = require('jquery');
+// var Backbone = require('backbone');
+// Backbone.$ = $;
+
+
+// $("ul").append("<li>Test</li>");
+$(function () {
+  // $("ul").find( "li" ).css( "background-color", "red" );
+  $('#asdf').css( 'background-color', 'red' );
+  $('#asdf').html('Changed!');
+  $('#insert').append('<li>Novo</li>');
+  console.log('begin');
+  console.log($('li'));
+  console.log('end');
+});
+
 
 var product = Backbone.Model.extend({
   defaults: {
@@ -21,11 +33,13 @@ products.fetch({
     console.log(products.length);
     console.log(Object.keys(products));
 //     console.log(todos.get(1).get('name')); // by id
-
     console.log(products.at(1).get('code')); // by index
     console.log(Object.keys(products.at(1).attributes));
     // console.log(todos.get(1).get('code')); // by id
     // console.log(todos.at(1).get('title')); // by index
+    var productView = new ProductView({model: products.at(1)});
+    console.log(productView.el);
+    $('#insert').append(productView.el);
   },
   error: function(model, xhr, options){
     // console.log(Object.keys(xhr).sort());
@@ -33,6 +47,19 @@ products.fetch({
     // console.log(xhr.responseText);
     // console.log(xhr.statusText);
     // console.log(options.error());
+    console.log('Error fetching data');
   }
 });
-console.log('reuqested');
+console.log('ptoducts reuqested...');
+
+var ProductView = Backbone.View.extend({
+  initialize: function () {
+    console.log('New view created.');
+    this.render();
+  },
+  tagName: 'li',
+  render: function(){
+    console.log(this.model.get('code'));
+    this.$el.html(this.model.get('code'));
+  }
+});
