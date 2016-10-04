ecd8252033b9444065ce0a075862f90cc45e9a7e
Douglas Gomes <douglasmg7@gmail.com>
2016-10-04 13:47:41 -0300

    Create products json to test.\nClean up and insert data to test.

--- server/test/tUpdateDbWithAllNationsProducts.js
+++ server/test/tUpdateDbWithAllNationsProducts.js
@@ -7,22 +7,39 @@ const expect = require('chai').expect;
 const mongo = require('mongodb').MongoClient;
 
 const mongoUrl = 'mongodb://localhost:27017/storeTest';
-const productsAllNations = require('./productsAllNations2.json');
+const productsAllNations = require('./AllNationsProducts.json');
 
-before(()=>{
-  // Pupulate db.
-  mongo.connect(mongoUrl, (err, db)=>{
-    expect(err).to.equal(null);
-    console.log(productsAllNations);
-    // console.log('test');
-    // db.collection('dealerProducts').insertMany(productsAllNations, (err, r)=>{
-    //   // expect(err).to.equal(null);
-    //   // expect(r)
-    // });
+
+
+describe('All Nations Products', function(){
+
+  before('Clean db test', (done)=>{
+    // Pupulate db.
+    // Convert timestamp string to date.
+    for (let val of productsAllNations){
+      val.ts = new Date(val.ts);
+    }
+    // Connect to db.
+    mongo.connect(mongoUrl, (err, db)=>{
+      expect(err).to.equal(null);
+      let col = db.collection('dealerProducts');
+      // Drop table.
+      col.drop(()=>{
+        // Assert table was dropped.
+        col.find().toArray((err, r)=>{
+          expect(r.length).to.equal(0);
+          // Insert All Nations products.
+          col.insertMany(productsAllNations, (err, r)=>{
+            expect(err).to.equal(null);
+            expect(r.insertedCount).to.equal(productsAllNations.length);
+          });
+          // db.collection('dealerProducts')
+          done();
+        });
+      });
+    });
   });
-});
 
-describe('Math', function(){
   it('returns the biggest number from arguments', function(){
     var max = Math.max(1 ,2 ,10, 3);
     expect(max).to.equal(10);
---
+++ server/test/util/createJsonProducts.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// Create a json file with All Nations porducts to be used in tests.
+// Discard the _id field.
+
+'use strict';
+
+const fs = require('fs');
+const assert = require('assert');
+const mongo = require('mongodb').MongoClient;
+
+const FILE_NAME = 'AllNationsProducts.json';
+// Amount of products to be created.
+const PRODUCT_QTD  = 20;
+
+// Discard _id field.
+let replace = function(k, v){
+  if(k === '_id'){
+    return undefined;
+  }
+  return v;
+};
+
+// Connect to db.
+mongo.connect('mongodb://localhost:27017/store', (err, db)=>{
+  assert.equal(null, err);
+  // Find All Nations products.
+  db.collection('dealerProducts').find().limit(PRODUCT_QTD).toArray((err, r)=>{
+    assert.equal(null, err);
+    db.close();
+    // Write All Nations products to json file.
+    fs.writeFile(FILE_NAME, JSON.stringify(r, replace, '  '), err=>{
+      assert.equal(null, err);
+      console.log(`All Nations products written to file: ${FILE_NAME}`);
+      console.log(`You shoud make manual changes to the created json file to perform desired test.`);
+      console.log(`The old file maybe alredy have the desired changes. Do not lost it!`);
+    });
+  });
+});
