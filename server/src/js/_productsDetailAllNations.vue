158db906ad50a10993450642656dfdc0fd110926
Douglas Gomes <douglasmg7@gmail.com>
2017-02-06 11:13:49 -0200

    testing pug loop

--- server/src/js/_productsDetailAllNations.vue
+++ server/src/js/_productsDetailAllNations.vue
@@ -4,7 +4,8 @@
       i.close.icon
       .header {{product.desc}}
       .content
-        table.ui.compact.striped.table
+        //- table.ui.compact.striped.table
+        table.ui.very.compact.striped.table
           tbody
             tr
               td Id All Nations
@@ -13,121 +14,84 @@
               td Id Loja
               td {{product.storeProductId ? product.storeProductId : ' - '}}
             tr
-              td Localização
-              td {{product.stockLocation}}
-            tr
               td Última atualização
               td {{product.ts}}
             tr
-              td Imagem
-              td
-                .image
-                  img(v-if="product.urlImg" @click="changePic()" v-bind:src="product.urlImg + '-0' + picId" v-bind:alt="product.desc")
+              td Fabricante
+              td {{product.manufacturer}}
             tr
               td Descrição
               td {{product.tecDesc}}
-        //- .ui.big.very.relaxed.list
-        .ui.divided.items
-          .item
-            .content
-              .header Id All Nations
-              .description {{product.code}}
-
-          .item
-            .content
-              .header Id Loja
-              .description {{product.storeProductId ? product.storeProductId : ' - '}}
-          .item
-            .content
-              .header Localização
-              .description {{product.stockLocation}}
-          .item
-            .content
-              .header Última atualização
-              .description {{product.ts}}
-          .item
-            .content
-              .header Ativo
-              .description {{product.active ? 'Ativo' : 'Inativo'}}
-          .item
-            .content
-              .header Disponibilidade
-              .description {{product.available ? 'Disponível' : 'Indisponível'}}
-          .item
-            .content
-              .header Estoque
-              .description {{product.stockQtd}}
-          .item
-            .content
-              .header Preço
-              .description {{product.price}}
+            tr
+              td teste
+              td
+                - var n=0;
+                  while n < 4
+                    p= n++
+            tr
+              td Imagem
+              td
+                .ui.tiny.images(v-if='product.urlImg')
+                  - for(var x=0; x<4; x++)
+                    img(v-bind:src="product.urlImg + '-0' + picId")
+                    //- img(v-bind:src="product.urlImg + '-0' + picId")
+                //- img.ui.small.image(v-if="product.urlImg" @click="changePic()" v-bind:src="product.urlImg + '-0' + picId")
+                //- img.ui.small.image(v-if="product.urlImg" @click="changePic()" v-bind:src="product.urlImg + '-0' + picId")
+            tr
+              td Disponibilidade
+              td {{product.available ? 'Disponível' : 'Indisponível'}}
+            tr
+              td Ativo
+              td {{product.active ? 'Ativo' : 'Inativo'}}
+            tr
+              td Estoque
+              td {{product.stockQtd}}
+            tr
+              td Localização
+              td {{product.stockLocation}}
+            tr
+              td Preço
+              td {{product.price}}
               //- .description {{product.price | currencyBr}}
-          .item
-            .content
-              .header Descrição
-              .description {{product.tecDesc}}
-          .item
-            .content
-              .header Departamento
-              .description {{product.department}}
-          .item
-            .content
-              .header Categoria
-              .description {{product.category}}
-          .item
-            .content
-              .header Sub-categoria
-              .description {{product.subCategory}}
-          .item
-            .content
-              .header Fabricante
-              .description {{product.manufacturer}}
-          .item
-            .content
-              .header Número do produto
-              .description {{product.partNum}}
-          .item
-            .content
-              .header EAN
-              .description {{product.ean}}
-          .item
-            .content
-              .header Garantia
-              .description {{product.warranty}}
-          .item
-            .content
-              .header Peso
-              .description {{product.weight}}kg
-          .item
-            .content
-              .header Largura
-              .description {{product.width}}cm
-          .item
-            .content
-              .header Altura
-              .description {{product.height}}cm
-          .item
-            .content
-              .header Profundidade
-              .description {{product.deep}}cm
-          .item
-            .content
-              .header Imagem
-              .description
-                .image
-                  img(v-if="product.urlImg" @click="changePic()" v-bind:src="product.urlImg + '-0' + picId" v-bind:alt="product.desc")
-          .item
-            .content
-              .header NCM
-              .description {{product.ncm}}
-          .item
-            .content
-              .header Imposto
-              .description {{product.taxReplace ? 'Incide ICMS ST' : 'Não incide ICMS ST'}}
-          .item
-            .content
-              .header Origem
-              .description {{product.origin}}
+            tr
+              td Departamento
+              td {{product.department}}
+            tr
+              td Categoria
+              td {{product.category}}
+            tr
+              td Sub-categoria
+              td {{product.subCategory}}
+            tr
+              td Número do produto
+              td {{product.partNum}}
+            tr
+              td EAN
+              td {{product.ean}}
+            tr
+              td Garantia
+              td {{product.warranty}}
+            tr
+              td Peso
+              td {{product.weight}} kg
+            tr
+              td Largura
+              td {{product.width}} cm
+            tr
+              td Altura
+              td {{product.height}} cm
+            tr
+              td Profundidade
+              td {{product.deep}} cm
+            tr
+              td NCM
+              td {{product.ncm}}
+            tr
+              td Imposto
+              td {{product.taxReplace ? 'Incide ICMS ST' : 'Não incide ICMS ST'}}
+            tr
+              td Origem
+              td {{product.origin}}
       .actions
         .ui.black.cancel.button Fechar
         .ui.positive.button Salvar
