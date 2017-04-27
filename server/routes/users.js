71b6945f4ae68fdf8c51dd4544f4c9f0f93e0cd7
Douglas Gomes <douglasmg7@gmail.com>
2017-04-27 16:14:57 -0300

    passport config - continue 5

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,14 +9,13 @@ router.get('/login', (req, res, next)=>{
   console.log(`req.isAuthenticated: ${req.isAuthenticated()}`);
   // console.log(`cookies: ${JSON.stringify(req.cookies)}`);
   console.log(`session: ${JSON.stringify(req.session)}`);
-  if (!req.session.cart) {
-    req.session.cart = 1;
-  } else {
-    req.session.cart += 1;
-  }
   console.log(`signed cookies: ${JSON.stringify(req.signedCookies)}`);
+  console.log(`req.user: ${JSON.stringify(req.user)}`);
   res.render('login');
 });
+
+// login
+router.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect: 'login'}));
 
 // logout
 router.post('/logout', (req, res, next)=>{
@@ -28,12 +27,43 @@ router.post('/logout', (req, res, next)=>{
   res.json({ success: true, message: 'Logout.' });
 });
 
+// sign up
+router.post('/sign-up', (req, res, next)=>{
+  if (req.body.username && req.body.password) {
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      admin: false
+    };
+    mongo.db.collection(dbConfig.collSession).insertOne(user)
+    .then(result=>{
+      res.json({ success: true, message: 'Sign up successfully accomplished' });
+    })
+    .catch(err=>{
+      res.json({ success: false, message: 'Sign up failed' });
+      console.log(`sign-up-error: ${err}`);
+    });
+  }
+});
+
+router.get('/test', (req, res, next)=>{
+  console.log(`cookies: ${JSON.stringify(req.cookies)}`);
+  console.log(`session: ${JSON.stringify(req.session)}`);
+  console.log(`signed cookies: ${JSON.stringify(req.signedCookies)}`);
+  let user = 'no user';
+  if (req.user) { user = req.user.username; }
+  res.render('test', {user: user});
+});
+
+// user logged
+router.get('/user', (req, res, next)=>{
+  res.render('user');
+});
+
 // login
 // router.post('/login', (req, res, next)=>{
 //   res.json({ success: false, message: 'Authentication failed. No user or password' });
 // });
-
-router.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect: 'login'}));
 
 // router.post('/login', function (req, res, next){
 //   console.log(`body: ${JSON.stringify(req.body)}`);
@@ -54,88 +84,4 @@ router.post('/login', passport.authenticate('local', {successRedirect: '/', fail
 //   res.redirect('/user/' + req.user.username);
 // });
 
-// login-old
-router.post('/login-old', (req, res, next)=>{
-  console.log(`cookies-1: ${req.cookies}`);
-  if (req.body.email && req.body.password) {
-    mongo.db.collection(dbConfig.collUsers).findOne({email: req.body.email})
-    .then(user=>{
-      // console.log(JSON.stringify(user));
-      // user not found
-      if (!user) {
-        res.json({ success: false, message: 'Authentication failed. User not found.' });
-      } else {
-        // wrong password
-        if (user.password !== req.body.password) {
-          res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-        // success
-        } else {
-          res.json({ success: true, message: 'Authentication success.', user: user.email });
-        }
-      }
-    })
-    .catch(err=>{
-      console.log(`POST /login err: ${err}`);
-    });
-  } else {
-    res.json({ success: false, message: 'Authentication failed. No user or password' });
-  }
-});
-
-// sign up
-router.post('/sign-up', (req, res, next)=>{
-  if (req.body.username && req.body.password) {
-    const user = {
-      username: req.body.username,
-      id: '---' + req.body.username + '---',
-      password: req.body.password,
-      admin: false
-    };
-    mongo.db.collection(dbConfig.collSession).insertOne(user)
-    .then(result=>{
-      res.json({ success: true, message: 'Sign up successfully accomplished' });
-    })
-    .catch(err=>{
-      res.json({ success: false, message: 'Sign up failed' });
-      console.log(`sign-up-error: ${err}`);
-    });
-  }
-});
-
-// token parse
-// router.use(function (req, res, next) {
-//   // check header or url parameters or post parameters for token
-//   const token = req.body.token || req.query.token || req.headers['x-access-token'];
-//   if (token) {
-//     jwt.verify(token, req.app.get('secret'), function (err, decoded) {
-//       if (err) {
-//         return res.json({ success: false, message: 'Failed to authenticate token.' });
-//       } else {
-//         req.decoded = decoded;
-//         next();
-//       }
-//     });
-//   // no token received
-//   } else {
-//     return res.status(403).send({success: false, message: 'No token provided.' });
-//   }
-// });
-// test
-router.get('/test', (req, res, next)=>{
-  console.log(`cookies: ${JSON.stringify(req.cookies)}`);
-  console.log(`session: ${JSON.stringify(req.session)}`);
-  console.log(`signed cookies: ${JSON.stringify(req.signedCookies)}`);
-  // const sess = req.session;
-  // let user = 'no user';
-  // if (sess.email) {
-  //   user = sess.email;
-  // }
-  // res.render('test', {user: user});
-  res.render('test', {email: req.session.email, password: req.session.password});
-});
-
-// user logged
-router.get('/user', (req, res, next)=>{
-  res.render('user');
-});
 module.exports = router;
